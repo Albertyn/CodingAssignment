8da1fdaac7c3008242beedaac014ca4d3982e4b0
George Albertyn <george.albertyn@outlook.com>
2018-07-20 23:39:15 +0200

    Add project files.

---
+++ CodingAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
